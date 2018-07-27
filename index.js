f859fac4a508bb8b8bb5eeb1f926626c1d175af3
Ryan Yang <Ryanyang99@hotmail.com>
2018-07-27 09:25:32 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3000
+const dotenv = require('dotenv').config();
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+const axios = require('axios');
+const request = require('request-promise');
+var cors = require('cors');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser')
+//mandrill
+var nodemailer = require('nodemailer');
+var mandrillTransport = require('nodemailer-mandrill-transport');
+//firebase
+var admin = require("firebase-admin");
+//var serviceAccount = require("./key.json");
+
+const app = express();
+const shopifyApiPublicKey = process.env.SHOPIFY_API_PUBLIC_KEY;
+const shopifyApiSecretKey = process.env.SHOPIFY_API_SECRET_KEY;
+const scopes = 'write_products';
+const appUrl = 'https://preorder-app.herokuapp.com/';
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false })) 
+
+app.use(bodyParser.json());
+
+
+//enable CORS 
+app.use(cors())
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
