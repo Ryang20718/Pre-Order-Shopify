c7a2b60bbecd688649208aca53dca8df5a0c6d0e
Ryan Yang <Ryanyang99@hotmail.com>
2018-07-27 09:39:24 -0700

    update

--- index.js
+++ index.js
@@ -64,7 +64,102 @@ const fetchShopData = async (shop, accessToken) => await axios(buildShopDataRequ
   }
 });
 
+///////////// Route Handlers /////////////
 
+
+app.post('/email', cors(), function(req, res){
+    getReceiver(req.body.email,req.body.message);
+    vesselMandrill(req.body.email,req.body.message);
+    console.log(req.body.email);
+    res.send('Mail Has Been Sent!')
+});
+
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+
+  if (!shop) { return res.status(400).send('no shop')}
+
+  const state = nonce();
+
+  const installShopUrl = buildInstallUrl(shop, state, buildRedirectUri())
+
+  res.cookie('state', state) // should be encrypted in production
+  res.redirect(installShopUrl);
+});
+/*
+app.get('/shopify/callback', async (req, res) => {
+  const { shop, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+  if (state !== stateCookie) { return res.status(403).send('Cannot be verified')}
+  const { hmac, ...params } = req.query
+  const queryParams = querystring.stringify(params)
+  const hash = generateEncryptedHash(queryParams)
+  if (hash !== hmac) { return res.status(400).send('HMAC validation failed')}
+  try {
+    const data = {
+      client_id: shopifyApiPublicKey,
+      client_secret: shopifyApiSecretKey,
+      code
+    };
+    const tokenResponse = await fetchAccessToken(shop, data)
+    const { access_token } = tokenResponse.data
+    const shopData = await fetchShopData(shop, access_token)
+    res.send(shopData.data.shop)
+  } catch(err) {
+    console.log(err)
+    res.status(500).send('something went wrong')
+  }
+    
+});
+*/
+
+app.get('/shopify/callback', (req,res) => {
+    const {shop,hmac,code,state} = req.query;
+    const stateCookie = cookie.parse(req.headers.cookie).state;
+    
+    if(state !== stateCookie){
+        return res.status(403).send('Request origin cannot be verified');
+    }
+    if(shop && hmac && code){
+        const map = Object.assign({},req.query);
+        delete map['hmac'];
+        const message = querystring.stringify(map);
+        const generatedHash = crypto.createHmac('sha256',shopifyApiSecretKey)
+        .update(message).digest('hex');
+        
+        if(generatedHash !== hmac){
+            return res.status(400).send('HMAC validation failed');
+        }
+        const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+        const accessTokenPayLoad = {
+            client_id:shopifyApiPublicKey,
+            client_secret:shopifyApiSecretKey,
+            code
+        };
+        
+        request.post(accessTokenRequestUrl,{json: accessTokenPayLoad})
+        .then((accessTokenResponse) => {
+            const accessToken = accessTokenResponse.access_token;
+            
+            const apiRequestUrl = 'https://' + shop + '/admin/shop.json';// GET URL
+            const apiRequestHeader = {
+                'X-Shopify-Access-Token': accessToken
+            };
+            request.get(apiRequestUrl,{headers: apiRequestHeader})
+            .then((apiResponse) =>{
+                res.end(apiResponse);
+            })
+            .catch((error) => {
+                res.status(error.statusCode).send(error.error.error_description);
+            });
+        })
+        .catch((error) =>{
+            res.status(error.statusCode).send(error.error.error_description);
+        });
+    }else{
+        res.status(400).send('Required Parameters missing');
+    }
+});
 
 //functions to send mail through regular gmail in case Mandrill runs out of mail sends
 
